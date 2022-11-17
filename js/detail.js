6de920502b0cddac34c9ff8fb4828432a158156f
Valentin Quiroga <valegolquiroga@gmail.com>
2022-11-17 19:51:17 -0300

    retoques en home, index y detail

--- js/detail.js
+++ js/detail.js
@@ -1,3 +1,7 @@
+import {createFooter, createUserNavbar} from "./helper-functions.js";
+createFooter()
+createUserNavbar()
+
 const id = window.location.hash.slice(1);
 console.log(id);
 const doctors2 = JSON.parse(localStorage.getItem('doctors'));
@@ -5,7 +9,7 @@ console.log(doctors2);
 const doctor = doctors2.find(doctor=> doctor.id == id);
     console.log(doctor);
     
-const doctorDetail = document.createElement('div');
+const doctorDetail = document.createElement('div');    
     
 // doctorDetail.id = doctor.id;
 
@@ -14,14 +18,14 @@ doctorDetail.classList.add('row', "test");
 doctorDetail.innerHTML=`
 <div class="row">
 <div class="col-xl-6 col-lg-6">
-<div class="image-doctor"> <img src="${doctor.imagen}" class="img-fluid" alt="Dr Valentin E. Galarga"/> </div> 
+<div class="image-doctor"> <img src="${doctor.imagen}" class="card-imagen" alt="Dr Valentin E. Galarga"/> </div> 
 </div>
 <div class="col-xl-6 col-lg-6 mt-5">
 <div class="row">
 <div class="col">
                     <h1 class="text-center">${doctor.name}</h1>
-                </div>
-            </div>
+                </div>    
+            </div>    
             <div class="row">
                 <div class="col">
                     <p class="text-center">${doctor.descripcion}</p>
@@ -33,8 +37,8 @@ doctorDetail.innerHTML=`
                                 <th scope="col" class="text-center">Dias</th>
                                 <th colspan=10 class="text-center">Horarios de Atención</th>
                               
-                              </tr>
-                            </thead>
+                              </tr>  
+                            </thead>  
                             <tbody>
                               <tr>
                                 
@@ -50,7 +54,7 @@ doctorDetail.innerHTML=`
                                <td>11:40</td>
                                <td>12:00</td>
                             
-                              </tr>
+                              </tr> 
                               <tr>
                                 
                                <td class="text-center">Miércoles</td>
@@ -64,7 +68,7 @@ doctorDetail.innerHTML=`
                                <td>11:20</td>
                                <td>11:40</td>
                                <td>12:00</td>
-                              </tr>
+                              </tr> 
                               <tr>
                                
                                <td class="text-center">Viernes</td>
@@ -78,17 +82,18 @@ doctorDetail.innerHTML=`
                                <td>11:20</td>
                                <td>11:40</td>
                                <td>12:00</td>
-                              </tr>
-                            </tbody>
-                          </table>
-                        </div>
+                              </tr> 
+                            </tbody>  
+                          </table>  
+                        </div>  
                         <button type="button" class="btn btn-info btn-lg">Solicitá tu turno online</button>
                         
                 
-            </div>
-        </div>
-    </div>
+            </div>            
+        </div>    
+    </div>    
     `
-let doctorContainer = document.querySelector('#doctor-container');
+let doctorContainer = document.querySelector('#doctor-container');    
 
 doctorContainer.appendChild(doctorDetail);
+
