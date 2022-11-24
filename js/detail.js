290aa40130f82fc1db16037b0f167b3eb43311ae
Valentin Quiroga <valegolquiroga@gmail.com>
2022-11-24 17:21:29 -0300

    terminando turnero

--- js/detail.js
+++ js/detail.js
@@ -15,12 +15,32 @@ console.log(doctors2);
 const doctor = doctors2.find(doctor=> doctor.id == id);
     console.log(doctor);
     
+
+    class Doctor{
+      constructor(id,name, especialidad, descripcion, direccion, imagen, telefono,  matricula , turnos,publicado){
+      this.id = id
+      this.name = name
+      this.especialidad = especialidad
+      this.descripcion = descripcion
+      this.direccion = direccion 
+      this.imagen=imagen
+      this.telefono = telefono 
+      this.matricula = matricula 
+      this.turnos=turnos
+      this.publicado = publicado
+      }
+    }
+
+   class Turno{
+    constructor(hora,disponible){
+      this.hora=hora
+      this.disponible=disponible
+    }
+   }
+    // CREACION DINAMICA DE PAGINA DE DETALLE
+
 const doctorDetail = document.createElement('div');    
-    
-// doctorDetail.id = doctor.id;
-
 doctorDetail.classList.add('row', "test");
-
 doctorDetail.innerHTML=`
 <div class="row">
 <div class="col-xl-6 col-lg-6">
@@ -56,7 +76,7 @@ doctorDetail.innerHTML=`
 
                                     
                                     `
-                                    <td class=${turno.disponible?"bg-success":"bg-danger"}>${turno.hora}</td>
+                                    <td class=${turno.disponible?"bg-success":"bg-secondary"}>${turno.hora}</td>
                                     
                                     `
                                     )
@@ -72,7 +92,7 @@ doctorDetail.innerHTML=`
 
                                   
                                   `
-                                  <td class=${turno.disponible?"bg-success":"bg-danger"}>${turno.hora}</td>
+                                  <td class=${turno.disponible?"bg-success":"bg-secondary"}>${turno.hora}</td>
                                   
                                   `
                                   )
@@ -88,7 +108,7 @@ doctorDetail.innerHTML=`
 
                                   
                                   `
-                                  <td class=${turno.disponible?"bg-success":"bg-danger"}>${turno.hora}</td>
+                                  <td class=${turno.disponible?"bg-success":"bg-secondary"}>${turno.hora}</td>
                                   
                                   `
                                   )
@@ -107,13 +127,12 @@ doctorDetail.innerHTML=`
     </div>    
     `
 let doctorContainer = document.querySelector('#doctor-container');    
-
 doctorContainer.appendChild(doctorDetail);
 
 // A la tabla se le podria poner colores dependiendo si el turno esta libre y en el modal de solicita tu turno rellenar los campos teniendo en cuenta los datos del usuario loggeado
-// const userToFill = userLogged
-// console.log(userToFill);
 
+
+//CREACION DINAMICA DEL MODAL
 const modalTurno = document.createElement("div")
 modalTurno.classList.add("modal","fade")
 modalTurno.setAttribute("tabindex","-1")
@@ -181,58 +200,70 @@ modalTurno.innerHTML=`
 </div>
 </div>
 `
-//data-bs-dismiss="modal" aria-label="Close"
 const modalContainer = document.getElementById("open-modal-container")
 modalContainer.appendChild(modalTurno)
-const consultasLunes = []
-const consultasMiercoles = []
-const consultasViernes = [] 
-class Consulta{
-  constructor(id,name,email,dni,motivo,dia,hora,obra){
-  this.id=id
-  this.name = name
-  this.email = email
-  this.dni=dni
-  this.motivo=motivo
-  this.dia=dia
-  this.hora=hora
-  this.obra=obra
-  }
-}
-const pedirturno = ()=>{
-const name = document.getElementById("values-name-turn").value 
-const dni = document.getElementById("values-dni-turn").value 
-const email = document.getElementById("values-email-turn").value 
-const motivo = document.getElementById("values-motivo-turn").value 
-const dia = document.getElementById("values-dia-turn").value 
-const hora = document.getElementById("values-hora-turn").value 
-const obra = document.getElementById("values-obra-turn").value 
-const newConsulta = new Consulta(new Date().getTime(),name,email,dni,motivo,dia,hora,obra)
 
-if (dia=="Lunes") {
-  consultasLunes.push(newConsulta)
-  localStorage.setItem("consultasLunes",JSON.stringify(consultasLunes))
-  
-} else if(dia=="Miercoles"){
-  consultasMiercoles.push(newConsulta)
-  localStorage.setItem("consultasMiercoles",JSON.stringify(consultasMiercoles))
-}else{
-  consultasViernes.push(newConsulta)
-  localStorage.setItem("consultasViernes",JSON.stringify(consultasViernes))
+// class Consulta{
+//   constructor(id,name,email,dni,motivo,dia,hora,obra){
+//   this.id=id
+//   this.name = name
+//   this.email = email
+//   this.dni=dni
+//   this.motivo=motivo
+//   this.dia=dia
+//   this.hora=hora
+//   this.obra=obra
+//   }
+// }
 
-}
-
+// FUNCION PARA SOLICITAR EL TURNO
+const pedirturno = (idToEdit)=>{
+const dia = document.getElementById("values-dia-turn").value
+const horario = document.getElementById("values-hora-turn").value 
+let position= 
 document.querySelector('#form-modal').reset();
 alert("Su consulta ha sido guardada con exito")
+if(dia.toLowerCase()=="lunes"){
+  position="lunes"
+}else if(dia.toLowerCase()=="miercoles"){
+  position="miercoles"
+}else{
+  position="viernes"
+}
+const turnoToChange=doctor.turnos[position]
+
+const finalTurn = turnoToChange.find(turno=>turno.hora=horario)
+finalTurn.disponible=false
+// const finalTurnNew= new Turno(horario,false)
+console.log(finalTurn);
+// console.log(finalTurnNew);
+//actualizando con el cambio del turno
+    const name = doctor.name
+    const especialidad = doctor.especialidad
+    const descripcion = doctor.descripcion
+    const direccion = doctor.direccion
+    const telefono = doctor.telefono
+    const matricula = doctor.matricula
+    const turnos=doctor.turnos
+    const imagen = doctor.imagen
+    const id = doctor.id
+    const publicado=doctor.publicado 
+  const doctorsUpdated = doctors2.filter(doctor => doctor.id != idToEdit);
+  const doctorUpdated = new Doctor(id, name,  especialidad, descripcion, direccion, imagen,  telefono,  matricula,  turnos,publicado);
+  doctorsUpdated.push(doctorUpdated);
+  localStorage.setItem("doctors", JSON.stringify(doctorsUpdated));
 }
 
 const formModal= document.getElementById("form-modal")
 formModal.onsubmit= pedirturno
+
+//FUNCION PARA RELLENAR LOS CAMPOS DEL MODAL ANTERIOR
 const fillmodal = () => {
-  
-  const userToFill = userLogged
+    const userToFill = userLogged
   document.getElementById("values-name-turn").value = userToFill.name
   document.getElementById("values-dni-turn").value = userToFill.dni
   document.getElementById("values-email-turn").value = userToFill.email
 }
 fillmodal()
+
+
