d24b9e69daff307817f219b0aaea33c83d97a8ce
Valentin Quiroga <valegolquiroga@gmail.com>
2022-11-24 18:13:40 -0300

    finalice turnero

--- js/detail.js
+++ js/detail.js
@@ -14,7 +14,7 @@ const doctors2 = JSON.parse(localStorage.getItem('doctors'));
 console.log(doctors2);
 const doctor = doctors2.find(doctor=> doctor.id == id);
     console.log(doctor);
-    
+
 
     class Doctor{
       constructor(id,name, especialidad, descripcion, direccion, imagen, telefono,  matricula , turnos,publicado){
@@ -22,10 +22,10 @@ const doctor = doctors2.find(doctor=> doctor.id == id);
       this.name = name
       this.especialidad = especialidad
       this.descripcion = descripcion
-      this.direccion = direccion 
+      this.direccion = direccion
       this.imagen=imagen
-      this.telefono = telefono 
-      this.matricula = matricula 
+      this.telefono = telefono
+      this.matricula = matricula
       this.turnos=turnos
       this.publicado = publicado
       }
@@ -39,94 +39,94 @@ const doctor = doctors2.find(doctor=> doctor.id == id);
    }
     // CREACION DINAMICA DE PAGINA DE DETALLE
 
-const doctorDetail = document.createElement('div');    
+const doctorDetail = document.createElement('div');
 doctorDetail.classList.add('row', "test");
 doctorDetail.innerHTML=`
 <div class="row">
 <div class="col-xl-6 col-lg-6">
-<div class="image-doctor"> <img src="${doctor.imagen}" class="img-fluid" alt="${doctor.name}"/> </div> 
+<div class="image-doctor"> <img src="${doctor.imagen}" class="img-fluid" alt="${doctor.name}"/> </div>
 </div>
 <div class="col-xl-6 col-lg-6 mt-5">
 <div class="row">
 <div class="col">
                     <h1 class="text-center">${doctor.name}</h1>
-                </div>    
-            </div>    
+                </div>
+            </div>
             <div class="row">
                 <div class="col">
-                    <div class="bg-body ">                
+                    <div class="bg-body ">
                     <p class=" lh-base">${doctor.descripcion}</p>
                     </div>
-                    <div class="table-responsive-xl"> 
+                    <div class="table-responsive-xl">
                         <table class="table table-bordered border-primary table-success table-responsive">
                             <thead>
                               <tr>
-                               
+
                                 <th scope="col" class="text-center">Dias</th>
                                 <th colspan=10 class="text-center">Horarios de Atención</th>
-                              
-                              </tr>  
-                            </thead>  
+
+                              </tr>
+                            </thead>
                             <tbody>
                               <tr>
-                                
+
                                <td class="text-center">Lunes</td>
                                 ${doctor.turnos.lunes.map(turno=>{
                                   return(
 
-                                    
+
                                     `
                                     <td class=${turno.disponible?"bg-success":"bg-secondary"}>${turno.hora}</td>
-                                    
+
                                     `
                                     )
-                                  
+
                                 }
                                   )}
-                              </tr> 
+                              </tr>
                               <tr>
-                                
+
                                <td class="text-center">Miércoles</td>
                                ${doctor.turnos.miercoles.map(turno=>{
                                 return(
 
-                                  
+
                                   `
                                   <td class=${turno.disponible?"bg-success":"bg-secondary"}>${turno.hora}</td>
-                                  
+
                                   `
                                   )
-                                
+
                               }
                                 )}
-                              </tr> 
+                              </tr>
                               <tr>
-                               
+
                                <td class="text-center">Viernes</td>
                                ${doctor.turnos.viernes.map(turno=>{
                                 return(
 
-                                  
+
                                   `
                                   <td class=${turno.disponible?"bg-success":"bg-secondary"}>${turno.hora}</td>
-                                  
+
                                   `
                                   )
-                                
+
                               }
                                 )}
-                              </tr> 
-                            </tbody>  
-                          </table>  
-                        </div>  
+                              </tr>
+                            </tbody>
+                          </table>
+                        </div>
                         <button type="button" class="btn-detail-get-turn" data-bs-toggle="modal" data-bs-target="#turn-modal">Solicitá tu turno online</button>
-                        
-                
-            </div>            
-        </div>    
-    </div>    
+
+
+            </div>
+        </div>
+    </div>
     `
-let doctorContainer = document.querySelector('#doctor-container');    
+let doctorContainer = document.querySelector('#doctor-container');
 doctorContainer.appendChild(doctorDetail);
 
 // A la tabla se le podria poner colores dependiendo si el turno esta libre y en el modal de solicita tu turno rellenar los campos teniendo en cuenta los datos del usuario loggeado
@@ -159,7 +159,7 @@ modalTurno.innerHTML=`
         <input type="email" class="form-control" id="values-email-turn" placeholder="name@example.com" maxlength="40" required>
         <label for="floatingInput">EMAIL</label>
       </div>
-       
+
       <div class="form-floating mb-3">
         <input type="text" class="form-control" id="values-obra-turn" placeholder="XX" max="120" min="16">
         <label for="floatingInput">OBRA SOCIAL</label>
@@ -217,26 +217,26 @@ modalContainer.appendChild(modalTurno)
 // }
 
 // FUNCION PARA SOLICITAR EL TURNO
-const pedirturno = (idToEdit)=>{
+const pedirturno = ()=>{
 const dia = document.getElementById("values-dia-turn").value
-const horario = document.getElementById("values-hora-turn").value 
-let position= 
+const horario = document.getElementById("values-hora-turn").value
+let day
 document.querySelector('#form-modal').reset();
 alert("Su consulta ha sido guardada con exito")
 if(dia.toLowerCase()=="lunes"){
-  position="lunes"
+  day="lunes"
 }else if(dia.toLowerCase()=="miercoles"){
-  position="miercoles"
+  day="miercoles"
 }else{
-  position="viernes"
+  day="viernes"
 }
-const turnoToChange=doctor.turnos[position]
+const turnoToChange=doctor.turnos[day]
 
-const finalTurn = turnoToChange.find(turno=>turno.hora=horario)
+const finalTurn = turnoToChange.find(turno=>turno.hora==horario)
 finalTurn.disponible=false
-// const finalTurnNew= new Turno(horario,false)
+
 console.log(finalTurn);
-// console.log(finalTurnNew);
+
 //actualizando con el cambio del turno
     const name = doctor.name
     const especialidad = doctor.especialidad
@@ -247,16 +247,18 @@ console.log(finalTurn);
     const turnos=doctor.turnos
     const imagen = doctor.imagen
     const id = doctor.id
-    const publicado=doctor.publicado 
-  const doctorsUpdated = doctors2.filter(doctor => doctor.id != idToEdit);
+    const publicado=doctor.publicado
+
+  const doctorsUpdated = doctors2.filter(doc => doc.id != doctor.id);
   const doctorUpdated = new Doctor(id, name,  especialidad, descripcion, direccion, imagen,  telefono,  matricula,  turnos,publicado);
   doctorsUpdated.push(doctorUpdated);
   localStorage.setItem("doctors", JSON.stringify(doctorsUpdated));
+  window.location.reload()
+  console.log("llegue");
 }
 
 const formModal= document.getElementById("form-modal")
 formModal.onsubmit= pedirturno
-
 //FUNCION PARA RELLENAR LOS CAMPOS DEL MODAL ANTERIOR
 const fillmodal = () => {
     const userToFill = userLogged
