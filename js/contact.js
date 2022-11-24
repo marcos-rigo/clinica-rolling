5c2998ce2487dc2bb69b6bed036cd1f3d4f43287
martin <martinfanlo31@gmail.com>
2022-11-23 21:30:35 -0300

    retoqué pagina de contacto

--- js/contact.js
+++ js/contact.js
@@ -1,8 +1,3 @@
-const  $form =document.querySelector ('#form')
- $form. addEventListener ('submit',handleSubmit)
-function handleSubmit(Event) {
-    Event.preventDefault()    
-}
 import {createFooter, createUserNavbar, createAdminNavbar} from "./helper-functions.js";
 const userLogged = JSON.parse(localStorage.getItem('userInfo'));
 if(userLogged.admin){
